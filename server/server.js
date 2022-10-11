ce2bb3efcb0cee4da4f1ce1757c15bbf28f4d3f2
Jimmy Solis <jimmysolis14@gmail.com>
2022-10-11 18:00:56 -0400

    finished server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001:
+const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
     typeDefs,
@@ -22,5 +22,19 @@ if ( process.env.NODE_ENV === 'production'){
 }
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
 })
+
+const startApolloServer = async (typeDefs, resolvers) => {
+    await server.start();
+    server.applyMiddleware({ app });
+
+    db.once('open', () => {
+        app.listen( PORT, () => {
+            console.log(`API server running on port ${PORT}! 🚨`):
+            console.log(`Use GraphQL at  http://localhost:${PORT}${server.graphqlPath}`);
+        })
+    })
+};
+
+startApolloServer(typeDefs,resolvers)
